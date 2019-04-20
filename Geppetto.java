574edc57a0bb250075051303ee2b20fd7e17384a
Nick-Stewart7 <49692388+Nick-Stewart7@users.noreply.github.com>
2019-04-20 13:56:26 -0400

    Update Geppetto.java

--- Geppetto.java
+++ Geppetto.java
@@ -6,16 +6,12 @@ import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
-
-import javax.sound.midi.ControllerEventListener;
-import javax.sound.midi.MetaEventListener;
 import javax.sound.midi.MetaMessage;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 import javax.sound.midi.ShortMessage;
-import javax.sound.midi.Soundbank;
 import javax.sound.midi.Synthesizer;
 import javax.sound.midi.Track;
 import javax.swing.*;
@@ -98,7 +94,6 @@ public class Geppetto {
 		jb.addActionListener(
 			ae -> {
 				try {
-					sequence = new Sequence(Sequence.PPQ, PPQ);
 					track = sequence.createTrack();
 					setChannel();
 					for (int i = 0; i < Grid.length; i++) {
